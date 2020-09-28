1f11930d9861fff2c945347efd37ec3db96b392b
finch <921146548@qq.com>
2020-09-28 15:16:49 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -26,9 +27,15 @@ class UsersController extends Controller
         return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,User $user,ImageUploadHandler $uploader)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if($request->avatar){
+           $result = $uploader->save($request->avatar,'avatar',$user->id);
+           if($result)
+               $data['avatar'] = $result['path'];
+        }
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 }
