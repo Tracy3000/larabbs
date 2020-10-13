8cc0bc389987dcc6c8f578cde6a20baf175c5caf
finch <921146548@qq.com>
2020-10-13 17:07:41 +0800

    用户话题列表

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -10,9 +10,8 @@ class CategoriesController extends Controller
 {
     public function show(Category $category, Topic $topic, Request $request)
     {
-        $topics = $topic->withOrder($request->order)
+        $topics = $topic->where('category_id',$category->id)->withOrder($request->order)
             ->with('category','user')
-            ->where('category_id',$category->id)
             ->paginate(20);
         // 传参变量话题和分类到模板中
         return view('topics.index', compact('topics', 'category'));
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,7 @@ class UsersTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(6);
         $user->name = 'Tracy';
-        $user->email = 'Tracy@example.com';
+        $user->email = '921146548@qq.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
