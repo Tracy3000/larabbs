4593b601d72cb110e95b4e141f96b43de2ac6fa9
finch <921146548@qq.com>
2020-10-21 16:03:51 +0800

    编辑话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -5,12 +5,16 @@ namespace App\Policies;
 use App\Models\User;
 use App\Models\Topic;
 
+/**
+ * Class TopicPolicy
+ * @package App\Policies 策略
+ */
 class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+         return $topic->user_id == $user->id;
+//        return true;
     }
 
     public function destroy(User $user, Topic $topic)
