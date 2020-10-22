87b788f49776e50118f300603f7dc85f27a8696b
finch <921146548@qq.com>
2020-10-22 15:52:23 +0800

    翻译 URL标示

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,8 @@ Route::resource('users','UsersController', ['only' => ['show', 'update', 'edit']
 
 
 //话题路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}','TopicsController@show')->name('topics.show');
 
 //分类路由
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
