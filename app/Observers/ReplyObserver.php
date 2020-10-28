a1a1b0cc21f3eed3f9c5af9903436d071fae59fc
finch <921146548@qq.com>
2020-10-28 16:53:16 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -26,11 +26,14 @@ class ReplyObserver
 //        $reply->topic()->increment('reply_count', 1);
 //        dd(\DB::getQueryLog());
 //        $reply->topic->increment('reply_count',1);
-
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $reply->topic->updateReplyCount();
 
         //通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
     }
 }
