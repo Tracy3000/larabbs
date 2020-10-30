c8c0264333d16c91f09c7ff8b2458ad9cbc526e7
finch <921146548@qq.com>
2020-10-30 14:21:34 +0800

    集成 administrator

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -15,9 +15,8 @@ class Policy
 
     public function before($user, $ability)
 	{
-	    //如果用户拥有内容管理权限授权通过
-	    if($user->can('manage_contents')){
+	    //如果用户拥有内容管理权限授权通过 true:通过，false:拒绝，null:通过其它的策略方法来决定授权通过与否
+	    if($user->can('manage_contents')){//所有权限在Gate注册，可以用can方法
 	        return true;
-
         }	}
 }
