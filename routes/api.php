2e39b79f07421ddb5411215db45b6087ba94ac7f
finch <921146548@qq.com>
2020-12-01 14:29:37 +0800

    资源推荐

--- routes/api.php
+++ routes/api.php
@@ -70,7 +70,8 @@ Route::prefix('v1')
                 //某个用户的回复
                 Route::get('users/{user}/replies','RepliesController@userIndex')->name('users.reolies.index');
 
-
+                //资源推荐
+                Route::get('links','LinksController@index')->name('links.index');
                 //登录后可以访问的接口
                 Route::middleware('auth:api')->group(function(){
                     //当前用户登录信息
