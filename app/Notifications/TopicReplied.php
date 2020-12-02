a7b6c4f3e939c61d30d63d85fd36f0548cedc9fe
finch <921146548@qq.com>
2020-12-02 15:10:36 +0800

    push notification

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -3,10 +3,12 @@
 namespace App\Notifications;
 
 use App\Models\Reply;
+use App\Notifications\Channels\JPushChannel;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use JPush\PushPayload;
 
 class TopicReplied extends Notification implements ShouldQueue
 {
@@ -33,7 +35,7 @@ class TopicReplied extends Notification implements ShouldQueue
      */
     public function via($notifiable)
     {
-        return ['database','mail'];
+        return ['database','mail',JPushChannel::class];
     }
 
     /**
@@ -78,5 +80,13 @@ class TopicReplied extends Notification implements ShouldQueue
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toJPush($notifiable, PushPayload $payload): PushPayload
+    {
+        return $payload
+            ->setPlatform('all')
+            ->addRegistrationId($notifiable->registration_id)
+            ->setNotificationAlert(strip_tags($this->reply->content));
     }
 }
